d04eabe3b0d66d8064d1c9d69f0ea60b8183d427
anandSgunti <agunti@mednethealth.com>
2025-07-08 17:06:13 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? 'EDetail-v1.0' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/EDetail-v1.0/' : '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
