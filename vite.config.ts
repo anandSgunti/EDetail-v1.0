be6f5a68eb5257f5205a3e13a4eb6a60e9ab02b7
anandSgunti <agunti@mednethealth.com>
2025-07-21 11:55:13 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/EDetail-v1.0/', // Always use this for GitHub Pages
+  base: '/', // Always use this for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
